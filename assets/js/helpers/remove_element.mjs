3ae61b0fdc41a1922f43bb3c4cda7fab0ae6e31c
Kohki SHIKATA <indigoworks@gmail.com>
2023-03-08 00:50:22 +0900

    Splitting JS functions

---
+++ assets/js/helpers/remove_element.mjs
@@ -0,0 +1,5 @@
+const removeElement = (i, formData) => {
+    formData.formElements.splice(i, 1)
+}
+
+export default removeElement
---
+++ assets/js/helpers/test_func.mjs
@@ -0,0 +1,5 @@
+const testFunc = data => {
+    console.log(data)
+}
+
+export default testFunc
