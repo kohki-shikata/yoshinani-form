2fdc53fdaafd5391c2c98e8f5caf6e5a8b93ad47
Kohki SHIKATA <indigoworks@gmail.com>
2023-03-08 06:22:46 +0900

    WIP - input screen enhancement

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 import Alpine from "https://cdn.skypack.dev/alpinejs@3.10.5";
 import component from "https://unpkg.com/alpinejs-component@latest/dist/component.esm.js"
+// import ja from "../i18n/ja.json"
+
+// Load multi lang data
+const jaData = await fetch('assets/i18n/ja.json')
+const ja = await jaData.json()
+const enData = await fetch('assets/i18n/en.json')
+const en = await enData.json()
 
 // Load variables and constants.
 import formElementTypes from "./constant/form_element_types.mjs"
@@ -14,6 +21,8 @@ import postData from "./helpers/remove_choice.mjs"
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('alpineCodebehind', () => ({
+        inputScreenSwitch: 'general',
+        sendMethod: 'smtp',
         types: formElementTypes,
         setType: 'text',
         formData: {
@@ -26,6 +35,22 @@ document.addEventListener('alpine:init', () => {
         removeChoice,
         postData,
 
+    }))
+
+    // i18n
+    Alpine.data('lang', () => ({
+        langCode: 'en',
+        langSet: {
+            en: en,
+            ja: ja,
+        },
+        toggle(lang) {
+            if (lang === 'en') {
+                this.langCode = 'ja'
+            } else {
+                this.langCode = 'en'
+            }
+        }
     }))
 })
 Alpine.plugin(component)
