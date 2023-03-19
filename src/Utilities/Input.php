2f19dd89a495e7d041a87812f1900bfaeae525db
Kohki SHIKATA <indigoworks@gmail.com>
2023-03-20 04:09:35 +0900

    Add screen setting on backend

--- src/Utilities/Input.php
+++ src/Utilities/Input.php
@@ -15,6 +15,28 @@ class Input extends BuildForm {
     parent::__construct();
   }
 
+  public function output_action() {
+    $setting = $this->screen_setting;
+    if($setting === 'input') {
+      return '';
+    } elseif($setting === 'confirm') {
+      return '/confirm';
+    } elseif($setting === 'complete') {
+      return '/send';
+    }
+  }
+
+  public function output_button_label() {
+    $setting = $this->screen_setting;
+    if($setting === 'input') {
+      return 'Send';
+    } elseif($setting === 'confirm') {
+      return 'Confirm';
+    } elseif($setting === 'complete') {
+      return 'Send';
+    }
+  }
+
   public function render() {
     $template = $this->twig->load('/page/input.html.twig');
     $data = [
@@ -23,6 +45,9 @@ class Input extends BuildForm {
       'csrf_token' => $this->output_csrf_token(),
       'form_settings' => $this->initial_settings,
       'form' => $this->loop_out(),
+      'screen_setting' => $this->screen_setting,
+      'action' => $this->output_action(),
+      'button_label' => $this->output_button_label(),
     ];
 
     return $template->render($data);
